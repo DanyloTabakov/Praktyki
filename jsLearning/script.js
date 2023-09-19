5bf780170be6dcfede141356626f1d9e1717534d
Cavabecho <115113171+Cavabecho@users.noreply.github.com>
2023-09-19 16:15:36 +0200

    I have been started learning js

---
+++ jsLearning/script.js
@@ -0,0 +1,28 @@
+/* Prymitywne typy danych */
+let power = 10
+let quantity = 18
+
+let information = "Ala ma kota"
+let numberAsString = "2.55"
+
+let isTrue = true
+let isFalse = false
+
+let empty = null
+let special = undefined
+
+// Złożone typy danych
+const character = {
+    power: 10,
+    name: "Ekko",
+    type: "Assasin",
+    age: undefined,
+    gender: "Male"
+}
+
+const usersID = [1, 5, 736, 21, 0]
+
+// --------------------------------------------------------
+let name = "Danylo"
+let typeOfName = typeof(name)
+console.log(typeOfName)
